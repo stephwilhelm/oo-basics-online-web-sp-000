64ee74569ac28580c404bcd1760865e2ab760f93
Steph W <wilhelm.steph@gmail.com>
2020-05-04 01:01:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
+class Shoe
+  attr_accessor :brand, :color, :size :material :condition
+  attr_reader :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
+
+end
 # Make your shoe class here!
